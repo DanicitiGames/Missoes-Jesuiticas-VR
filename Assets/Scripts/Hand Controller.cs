1ab3775accadf85052180408e5c6e15020df4418
Jonas Pinheiro <jonasespinheiro@gmail.com>
2025-11-26 15:39:09 -0300

    Fix: Arrumar bug no script da custom Hand

--- Assets/Scripts/Hand Controller.cs
+++ Assets/Scripts/Hand Controller.cs
@@ -1,44 +1,52 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class HandController : MonoBehaviour
 {
-    
     [SerializeField] InputActionReference gripInputAction;
     [SerializeField] InputActionReference triggerInputAction;
-    
+
     private Animator handAnimator;
 
-    private const string trigger = "Trigger";
-    private const string grip = "Grip";
+    private const string TRIGGER = "Trigger";
+    private const string GRIP = "Grip";
 
-    private void Awake() 
+    private void Awake()
     {
         handAnimator = GetComponent<Animator>();
     }
 
-    private void OnEnable() 
+    private void OnEnable()
     {
-        gripInputAction.action.performed += gripPressed;
-        triggerInputAction.action.performed += triggerPressed;
+        gripInputAction.action.Enable();
+        triggerInputAction.action.Enable();
 
+        gripInputAction.action.performed += GripPressed;
+        gripInputAction.action.canceled += GripPressed;
+
+        triggerInputAction.action.performed += TriggerPressed;
+        triggerInputAction.action.canceled += TriggerPressed;
     }
 
-    private void OnDisable() 
+    private void OnDisable()
     {
-        gripInputAction.action.performed -= gripPressed;
-        triggerInputAction.action.performed -= triggerPressed;
+        gripInputAction.action.Disable();
+        triggerInputAction.action.Disable();
+
+        gripInputAction.action.performed -= GripPressed;
+        gripInputAction.action.canceled -= GripPressed;
+
+        triggerInputAction.action.performed -= TriggerPressed;
+        triggerInputAction.action.canceled -= TriggerPressed;
     }
 
-    private void gripPressed(InputAction.CallbackContext obj) 
+    private void GripPressed(InputAction.CallbackContext ctx)
     {
-        handAnimator.SetFloat(grip, obj.ReadValue<float>());
+        handAnimator.SetFloat(GRIP, ctx.ReadValue<float>());
     }
 
-    private void triggerPressed(InputAction.CallbackContext obj) 
+    private void TriggerPressed(InputAction.CallbackContext ctx)
     {
-        handAnimator.SetFloat(trigger, obj.ReadValue<float>());
+        handAnimator.SetFloat(TRIGGER, ctx.ReadValue<float>());
     }
 }
